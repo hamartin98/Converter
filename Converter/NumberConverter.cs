ce98c1803dc28bdf428be2cc969f2fd6dd21041e
Martin <hazi.martin@gmail.com>
2021-03-23 09:37:21 +0100

    Moved number conversion logic to the NumberConverter class

---
+++ Converter/NumberConverter.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Windows;
+
+namespace Converter
+{
+    class NumberConverter
+    {
+        private const string hexLookup = "0123456789ABCDEF"; // Lookup table for number conversion
+        private const string romanLookup = "IVXLCDM"; // Lookup table for roman characters, ordered by value
+
+        // Converts a character to int value eg.: C -> 12
+        // Returns the integer value of the character
+        private static int CharToValue(char ch)
+        {
+            return (ch >= '0' && ch <= '9') ? Convert.ToInt32(ch - 48) : Convert.ToInt32(ch - 55);
+        }
+
+        // Convert and return the given number in base 10
+        // The number must be in Big-endian format
+        private static int ConvertToBase10(string number, int fromBase)
+        {
+            int result = 0;
+            int currValue = 0;
+
+            for (int idx = 0; idx < number.Length; idx++)
+            {
+                currValue = CharToValue(number[idx]); // get the integer value of the current character
+                result += Convert.ToInt32(currValue * Math.Pow(fromBase, idx)); // Add the current subresult to the result
+            }
+
+            return result;
+        }
+
+        // Converts the number between the given bases
+        // Returns the converted value formatted
+        public static string ConvertNumber(string number, int fromBase, int toBase)
+        {
+            string result = "";
+            int base10Num = ConvertToBase10(number, fromBase);
+
+            do
+            {
+                result += hexLookup[base10Num % toBase];
+                base10Num /= toBase;
+            }
+            while (base10Num > 0);
+
+            return FormatString(result);
+        }
+
+        // Converts the given number to Roman number
+        public static string ConvertToRomanNumber(string number, int fromBase)
+        {
+            // needs to be implemented
+            return "";
+        }
+
+        // Removes unnecessary spaces from the string
+        // Invert and convert the characters in the string to uppper case
+        // Returns the formatted string
+        public static string FormatString(string str)
+        {
+            string result = "";
+            str = str.Trim();
+
+            for (int idx = str.Length - 1; idx >= 0; idx--)
+            {
+                result += char.ToUpper(str[idx]);
+            }
+
+            return result;
+        }
+
+        // Checks if the given value is a valid number in the selected base
+        // Returns true if its correct
+        public static bool IsNumberValidInBase(string number, int baseOf)
+        {
+            string validChars = hexLookup.Substring(0, baseOf);
+
+            foreach (char ch in number)
+            {
+                if (!validChars.Contains(ch))
+                {
+                    MessageBox.Show("Number is invalid in the given base");
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- Converter/NumberConverterControl.xaml.cs
+++ Converter/NumberConverterControl.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -12,13 +11,11 @@ namespace Converter
     {
         private List<string> fromBaseList; // Stores the list of bases selectable to convert from
         private List<string> toBaseList; // Stores the list of bases selectable to convert to
-        private const string hexLookup = "0123456789ABCDEF"; // Lookup table for number conversion
-        private const string romanLookup = "IVXLCDM"; // Lookup table for roman characters, ordered by value
 
         public NumberConverterControl()
         {
             InitializeComponent();
-            InitList();
+            InitItemSources();
         }
 
         private void btnCopyResult_Click(object sender, RoutedEventArgs e)
@@ -42,84 +39,26 @@ namespace Converter
             int toBase;
             string toBaseStr = cbToBase.SelectedValue.ToString();
 
-            number = FormatString(number);
+            number = NumberConverter.FormatString(number);
 
-            if (number != "" && IsNumberValidInBase(number, fromBase))
+            if (number != "" && NumberConverter.IsNumberValidInBase(number, fromBase))
             {
                 if (toBaseStr == "Roman")
                 {
-                    //result = ConvertToRomanNumber(number, fromBase);
+                    result = NumberConverter.ConvertToRomanNumber(number, fromBase);
                 }
                 else
                 {
                     toBase = cbToBase.SelectedIndex + 2;
-                    result = FormatString(ConvertNumber(number, fromBase, toBase));
+                    result = NumberConverter.ConvertNumber(number, fromBase, toBase);
                 }
-            }
-
-            return result;
-        }
-
-        // Checks if the given value is a valid number in the selected base
-        // Returns true if its correct
-        private bool IsNumberValidInBase(string number, int baseOf)
-        {
-            string validChars = hexLookup.Substring(0, baseOf);
-            
-            foreach (char ch in number)
-            {
-                if (!validChars.Contains(ch))
-                {
-                    MessageBox.Show("Number is invalid in the given base");
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        // Converts the number between the given bases
-        // Returns the converted value
-        private string ConvertNumber(string number, int fromBase, int toBase)
-        {
-            string result = "";
-            int base10Num = ConvertToBase10(number, fromBase);
-
-            do
-            {
-                result += hexLookup[base10Num % toBase];
-                base10Num /= toBase;
-            }
-            while (base10Num > 0);
-
-            return result;
-        }
-
-        // Converts the given number to Roman number
-        private string ConvertToRomanNumber(string number, int fromBase)
-        {
-            // needs to be implemented
-            return "";
-        }
-
-        // Removes unnecessary spaces from the string
-        // Invert and convert the characters in the string to uppper case
-        // Returns the formatted string
-        private string FormatString(string str)
-        {
-            string result = "";
-            str = str.Trim();
-            
-            for(int idx = str.Length - 1; idx >= 0; idx--)
-            {
-                result += char.ToUpper(str[idx]);
             }
 
             return result;
         }
 
         // Initialize the base lists, then sets the itemsources of the base selector ComboBoxes
-        private void InitList()
+        private void InitItemSources()
         {
             fromBaseList = new List<string>();
             toBaseList = new List<string>();
@@ -133,29 +72,6 @@ namespace Converter
             cbFromBase.ItemsSource = fromBaseList;
             toBaseList.Add("Roman number");
             cbToBase.ItemsSource = toBaseList;
-        }
-
-        // Converts a character to int value eg.: C -> 12
-        // Returns the integer value of the character
-        private int CharToValue(char ch)
-        {
-            return (ch >= '0' && ch <= '9') ? Convert.ToInt32(ch - 48) : Convert.ToInt32(ch - 55);
-        }
-
-        // Convert and return the given number in base 10
-        // The number must be in Big-endian format
-        private int ConvertToBase10(string number, int fromBase)
-        {
-            int result = 0;
-            int currValue = 0;
-
-            for(int idx = 0; idx < number.Length; idx++)
-            {
-                currValue = CharToValue(number[idx]); // get the integer value of the current character
-                result += Convert.ToInt32(currValue * Math.Pow(fromBase, idx)); // Add the current subresult to the result
-            }
-
-            return result;
         }
     }
 }
